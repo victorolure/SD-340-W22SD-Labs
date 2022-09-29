83a3a21f27928da6f85a711393e465e21151840e
Victor Olukayode Olure <victorolure@gmail.com>
2022-09-28 19:45:45 -0500

    submit

--- SD-340-W22SD-Labs/Program.cs
+++ SD-340-W22SD-Labs/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using System.Text;
+
+string wordConcatenate(string wordArg, int numArg)
+{
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < numArg; i++)
+    {
+        sb.Append(wordArg);
+    }
+    return sb.ToString();
+}
+
+string testString = "My name is Victor";
+stringDelegate newDel = new stringDelegate(wordConcatenate);
+
+newDel = (string wordArg, int numArg) =>
+{
+    return wordArg.Substring(0, wordArg.Length - numArg);
+};
+
+Console.WriteLine(newDel(testString, 5));
+
+delegate string stringDelegate(string wordArg, int numArg);
